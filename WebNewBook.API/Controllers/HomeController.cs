c4baca9a0199a091eb34103730af39aafa2b4dda
hungldph13592 <hungldph13592@fpt.edu.vn>
2022-09-26 21:55:10 +0700

    update 2 folders

--- WebNewBook.API/Controllers/HomeController.cs
+++ WebNewBook.API/Controllers/HomeController.cs
@@ -16,64 +16,64 @@ namespace WebNewBook.API.Controllers
             _dbContext = dbContext;
         }
         
-        [HttpGet("HomeVM")]
-        public async Task<ActionResult<List<HomeVM>>> GetHomVM()
-        {
-            var sanPham = _dbContext.SanPhams.ToList();
-            var sanPhamCT = _dbContext.SanPhamCTs.ToList();
-            var sachCT = _dbContext.SachCTs.ToList();
-            var sach = _dbContext.Sachs.ToList();
-            var theLoai = _dbContext.TheLoais.ToList();
-            var danhMuc = _dbContext.DanhMucSachs.ToList();
-            List<HomeVM> homeVMs = new List<HomeVM>();
-            homeVMs = (from a in sanPham
-                      join b in sanPhamCT on a.ID_SanPham equals b.MaSanPham     
-                      join c in sachCT on b.MaSachCT equals c.ID_SachCT
-                      join d in sach on c.MaSach equals d.ID_Sach
-                      join e in theLoai on c.MaTheLoai equals e.ID_TheLoai
-                      join f in danhMuc on e.MaDanhMuc equals f.ID_DanhMuc
-                      select new HomeVM()
-                      {
-                          sanPhams = a,
-                          SanPhamCT = b,
-                          sachCT = c,
-                          sach = d,
-                          theLoai =e,
-                          danhMucSach = f,
-                      }).ToList();
+        //[HttpGet("HomeVM")]
+        //public async Task<ActionResult<List<HomeVM>>> GetHomVM()
+        //{
+        //    var sanPham = _dbContext.SanPhams.ToList();
+        //    var sanPhamCT = _dbContext.SanPhamCTs.ToList();
+        //    var sachCT = _dbContext.SachCTs.ToList();
+        //    var sach = _dbContext.Sachs.ToList();
+        //    var theLoai = _dbContext.TheLoais.ToList();
+        //    var danhMuc = _dbContext.DanhMucSachs.ToList();
+        //    List<HomeVM> homeVMs = new List<HomeVM>();
+        //    homeVMs = (from a in sanPham
+        //              join b in sanPhamCT on a.ID_SanPham equals b.MaSanPham     
+        //              join c in sachCT on b.MaSachCT equals c.ID_SachCT
+        //              join d in sach on c.MaSach equals d.ID_Sach
+        //              join e in theLoai on c.MaTheLoai equals e.ID_TheLoai
+        //              join f in danhMuc on e.MaDanhMuc equals f.ID_DanhMuc
+        //              select new HomeVM()
+        //              {
+        //                  sanPhams = a,
+        //                  SanPhamCT = b,
+        //                  sachCT = c,
+        //                  sach = d,
+        //                  theLoai =e,
+        //                  danhMucSach = f,
+        //              }).ToList();
             
-            return Ok(homeVMs);
+        //    return Ok(homeVMs);
               
-        }
-        [HttpGet("Product")]
-        public async Task<ActionResult<List<HomeVM>>> GetProduct()
-        {
-            var sanPham = _dbContext.SanPhams.ToList();
-            var sanPhamCT = _dbContext.SanPhamCTs.ToList();
-            var sachCT = _dbContext.SachCTs.ToList();
-            var sach = _dbContext.Sachs.ToList();
-            var theLoai = _dbContext.TheLoais.ToList();
-            var danhMuc = _dbContext.DanhMucSachs.ToList();
-            List<HomeVM> homeVMs = new List<HomeVM>();
-            homeVMs = (from a in sanPham
-                       join b in sanPhamCT on a.ID_SanPham equals b.MaSanPham
-                       join c in sachCT on b.MaSachCT equals c.ID_SachCT
-                       join d in sach on c.MaSach equals d.ID_Sach
-                       join e in theLoai on c.MaTheLoai equals e.ID_TheLoai
-                       join f in danhMuc on e.MaDanhMuc equals f.ID_DanhMuc
-                       select new HomeVM()
-                       {
-                           sanPhams = a,
-                           SanPhamCT = b,
-                           sachCT = c,
-                           sach = d,
-                           theLoai = e,
-                           danhMucSach = f,
-                       }).ToList();
+        //}
+        //[HttpGet("Product")]
+        //public async Task<ActionResult<List<HomeVM>>> GetProduct()
+        //{
+        //    var sanPham = _dbContext.SanPhams.ToList();
+        //    var sanPhamCT = _dbContext.SanPhamCTs.ToList();
+        //    var sachCT = _dbContext.SachCTs.ToList();
+        //    var sach = _dbContext.Sachs.ToList();
+        //    var theLoai = _dbContext.TheLoais.ToList();
+        //    var danhMuc = _dbContext.DanhMucSachs.ToList();
+        //    List<HomeVM> homeVMs = new List<HomeVM>();
+        //    homeVMs = (from a in sanPham
+        //               join b in sanPhamCT on a.ID_SanPham equals b.MaSanPham
+        //               join c in sachCT on b.MaSachCT equals c.ID_SachCT
+        //               join d in sach on c.MaSach equals d.ID_Sach
+        //               join e in theLoai on c.MaTheLoai equals e.ID_TheLoai
+        //               join f in danhMuc on e.MaDanhMuc equals f.ID_DanhMuc
+        //               select new HomeVM()
+        //               {
+        //                   sanPhams = a,
+        //                   SanPhamCT = b,
+        //                   sachCT = c,
+        //                   sach = d,
+        //                   theLoai = e,
+        //                   danhMucSach = f,
+        //               }).ToList();
 
-            return Ok(homeVMs);
+        //    return Ok(homeVMs);
 
-        }
+        //}
 
 
     }
