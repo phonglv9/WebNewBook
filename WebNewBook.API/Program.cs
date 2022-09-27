b0c8347d0f1a0ed0fe87930b73259f09bf8a1d7e
tuyen le <tuyenlhph15504@fpt.edu.vn>
2022-09-27 17:25:44 +0700

    hytdc

--- WebNewBook.API/Program.cs
+++ WebNewBook.API/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using WebNewBook.API.Data;
+using WebNewBook.API.Repository.IService;
+using WebNewBook.API.Repository.Service;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static System.Net.WebRequestMethods;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve); ;
+builder.Services.AddDbContext<dbcontext>(option => option.UseSqlServer("Data Source = 10.96.121.5; Initial Catalog = DATN; Integrated Security = False; user id = FPTShop; password = FPT$hop@^!^; Min Pool Size=20; Max Pool Size=2000; Connection Timeout = 30; Integrated Security = False; Persist Security Info=False; MultipleActiveResultSets = True; "));
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<dbcontext>(option => option.UseSqlServer("Data Source=LAPTOP-IOP6D48P\\SQLEXPRESS;Initial Catalog=FinalASM;User ID=hung;Password=hung;"));
