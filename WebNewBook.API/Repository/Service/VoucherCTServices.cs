6cfdcb89624562400b827becfcd877484dd7367b
cường lê <vancuong.tkt36@gmail.com>
2022-10-18 17:49:24 +0700

    18/10

---
+++ WebNewBook.API/Repository/Service/VoucherCTServices.cs
@@ -0,0 +1,60 @@
+﻿using WebNewBook.API.Data;
+using WebNewBook.API.Repository.IService;
+using WebNewBook.Model;
+
+namespace WebNewBook.API.Repository.Service
+{
+    public class VoucherCTServices : IVoucherCTServices
+    {
+        private readonly dbcontext _dbcontext;
+
+        public VoucherCTServices(dbcontext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task AddAutomaticallyAsync(VoucherCT voucherCT)
+        {
+            voucherCT.TrangThai = 0;
+            voucherCT.CreateDate = DateTime.Now;
+            voucherCT.NgayHetHan = voucherCT.Voucher.EndDate;
+            _dbcontext.Add(voucherCT);
+            await _dbcontext.SaveChangesAsync();
+        }
+
+        public Task AddImportExcerAsync(VoucherCT voucherCT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddManuallyAsync(VoucherCT voucherCT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VoucherCT?> GetVoucherByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VoucherCT>> GetVoucherChuaphathanhAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VoucherCT>> GetVoucherDaphathanhAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HuyVouCherAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PhathanhVouCherAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
