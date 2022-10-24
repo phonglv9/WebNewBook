08547df872c344530bbacb3ace73a54ff575120e
phonglv9 <phonglv9@fpt.com.vn>
2022-10-24 15:54:15 +0700

    donhangv1

---
+++ WebNewBook/Controllers/PaymentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+using WebNewBook.Models;
+
+namespace WebNewBook.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public List<CartItem> Giohangs
+        {
+            get
+            {
+                List<CartItem> data = new List<CartItem>();
+                var jsonData = Request.Cookies["Cart"];
+                if (jsonData != null)
+                {
+                    data = JsonConvert.DeserializeObject<List<CartItem>>(jsonData);
+                }             
+                return data;
+            }
+        }
+        public IActionResult CheckOut() {
+
+            ViewBag.Cart =  Giohangs;
+            ViewBag.TongTien = Giohangs.Sum(c => c.ThanhTien);
+
+
+          
+            return View();
+        }
+    }
+}
