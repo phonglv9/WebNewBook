4e510bedfcd006d1c59569a0e6b453f02c953df0
cường lê <vancuong.tkt36@gmail.com>
2022-10-26 10:44:37 +0700

    Ngày 26/10 cháy dự án cùng nhau đê (Try_cath)

--- WebNewBook.API/Repository/Service/CustomerService.cs
+++ WebNewBook.API/Repository/Service/CustomerService.cs
@@ -16,50 +16,90 @@ namespace WebNewBook.API.Repository.Service
 
         public async Task AddKhachHangAsync(KhachHang khachHang)
         {
-            khachHang.ID_KhachHang = Guid.NewGuid().ToString();
-            khachHang.TrangThai = 1;
-            _dbcontext.Add(khachHang);
-            await _dbcontext.SaveChangesAsync();
+            try
+            {
+                khachHang.ID_KhachHang = Guid.NewGuid().ToString();
+                khachHang.TrangThai = 1;
+                _dbcontext.Add(khachHang);
+                await _dbcontext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
         public async Task DeleteKhachHangAsync(string id)
         {
-            KhachHang? khachhang = _dbcontext.KhachHangs.FirstOrDefault(c => c.ID_KhachHang == id) ?? null;
-            if (khachhang != null)
+            try
             {
-                if (khachhang.TrangThai==1)
+                KhachHang? khachhang = _dbcontext.KhachHangs.FirstOrDefault(c => c.ID_KhachHang == id) ?? null;
+                if (khachhang != null)
                 {
-                    khachhang.TrangThai = 0;
-                    _dbcontext.KhachHangs.Update(khachhang);
-                    await _dbcontext.SaveChangesAsync();
+                    if (khachhang.TrangThai == 1)
+                    {
+                        khachhang.TrangThai = 0;
+                        _dbcontext.KhachHangs.Update(khachhang);
+                        await _dbcontext.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        khachhang.TrangThai = 1;
+                        _dbcontext.KhachHangs.Update(khachhang);
+                        await _dbcontext.SaveChangesAsync();
+                    }
+
                 }
-              
-else                {
-                    khachhang.TrangThai = 1;
-                    _dbcontext.KhachHangs.Update(khachhang);
-                    await _dbcontext.SaveChangesAsync();
-                }
-              
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
             }
         }
 
         public async Task<KhachHang?> GetKhachHangByIdAsync(string id)
         {
-            return await _dbcontext.KhachHangs.FirstOrDefaultAsync(c => c.ID_KhachHang == id) ?? null;
+            try
+            {
+                return await _dbcontext.KhachHangs.FirstOrDefaultAsync(c => c.ID_KhachHang == id) ?? null;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
-        public async Task<IEnumerable<KhachHang>> GetKhachHangsAsync(string search , int? status)
+        public async Task<IEnumerable<KhachHang>> GetKhachHangsAsync(string search, int? status)
         {
-            var model= await _dbcontext.KhachHangs.Where(c =>(status !=null ? c.TrangThai==status:true)&& ((!string.IsNullOrEmpty(search)? c.HoVaTen.ToLower().Contains(search) :true) || (!string.IsNullOrEmpty(search) ? c.Email.ToLower().Contains(search) : true) || (!string.IsNullOrEmpty(search) ? c.SDT.ToLower().Contains(search) : true)
-                                                                              )).ToListAsync();
-            
-            return model;
+            try
+            {
+                var model = await _dbcontext.KhachHangs.Where(c => (status != null ? c.TrangThai == status : true) && ((!string.IsNullOrEmpty(search) ? c.HoVaTen.ToLower().Contains(search) : true) || (!string.IsNullOrEmpty(search) ? c.Email.ToLower().Contains(search) : true) || (!string.IsNullOrEmpty(search) ? c.SDT.ToLower().Contains(search) : true)
+                                                                                    )).ToListAsync();
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
         public async Task UpdateKhachHangAsync(KhachHang khachHang)
         {
-            _dbcontext.Update(khachHang);
-            await _dbcontext.SaveChangesAsync();
+            try
+            {
+                _dbcontext.Update(khachHang);
+                await _dbcontext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
