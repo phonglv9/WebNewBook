572d4a7eb6482be1692d9dd9fce4489820d3acfa
Phong <lovanphong2299@gmail.com>
2022-11-27 17:48:15 +0700

    UpdateGUI:Login,Đăng ký quên mk, validate payment v2, xong màn thanh toán

---
+++ WebNewBook/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNewBook.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        
+    }
+}
