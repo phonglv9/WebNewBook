56d43414b77c753bc25f9da4042f5035cee0b037
Phong <lovanphong2299@gmail.com>
2022-11-30 21:26:43 +0700

    ok

---
+++ WebNewBook/Controllers/QLPhieuTraController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNewBook.Controllers
+{
+    public class QLPhieuTraController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.TitleAdmin = "TRẢ HÀNG";
+            return View();
+        }
+    }
+}
