6c2dc427ea889b3fa23330b18aa3650b96c750be
tuyen le <tuyenlhph15504@fpt.edu.vn>
2022-12-05 19:08:22 +0700

    fix sanphamCT

--- WebNewBook/Program.cs
+++ WebNewBook/Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=HoaDon}/{action=Index}/{id?}");
 
 app.Run();
